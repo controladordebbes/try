b3666c89c1f160cc0cded05bb470f4f94b7d9a6a
controladordebbes <154927045+controladordebbes@users.noreply.github.com>
2023-12-26 20:55:53 +0000

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,9 @@
+//Intro 
+$('#intro1').animate({'opacity':1} , 1000);
+$('#intro2').delay(750).animate({'opacity':1} , 1000);
+$('#intro3').delay(1500).animate({'opacity':1} , 1000);
+$('#intro').delay(3000).fadeOut(800);$('#intro1').animate({'opacity':1} , 1000);
+$('#intro2').delay(750).animate({'opacity':1} , 1000);
+$('#intro3').delay(1500).animate({'opacity':1} , 1000);
+$('#intro').delay(3000).fadeOut(800);
+
